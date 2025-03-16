7f2d571f41a2ac04462cc15e8ff95167004b817b
VICXPTRV <viktoriavorontsova.main@gmail.com>
2025-03-16 19:08:49 +0700

    add system delay measurement

---
+++ measure_delay.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Text Styles
+BOLD="\e[1m"
+RESET="\033[0m"
+# Colors
+BLACK="\033[38;5;0m"        # Black
+GREY="\033[38;5;250m"
+DARK_BLUE="\033[38;5;25m"   # Dracula-inspired dark blue
+LIGHT_BLUE="\033[38;5;39m"  # Bright but not overwhelming light blue
+PINK="\033[38;5;213m"       # Soft pink
+RED="\033[38;5;196m"        # Bright red for errors or warnings
+ORANGE="\033[38;5;208m"     # Vibrant orange
+YELLOW="\033[38;5;220m"     # Warm yellow
+GREEN="\033[38;5;40m"       # Fresh green
+WHITE="\033[38;5;15m"       # Pure white
+PURPLE="\033[38;5;129m"     # Dracula-inspired purple (optional addition)
+
+echo '#include <unistd.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    if (argc > 1) usleep(atoi(argv[1]));
+    return 0;
+}' > usleep.c
+
+cc usleep.c -o usleep
+
+# Function to measure the delay of usleep
+measure() {
+    start=$(date +%s%N)  # Get start time in nanoseconds
+    ./usleep 200000  # Sleep for 200 milliseconds
+    end=$(date +%s%N)  # Get end time in nanoseconds
+    delay=$(( (end - start) / 1000000 ))  # Calculate delay in milliseconds
+    echo $((delay - 200))  # Subtract the expected 200ms and return the delay
+}
+
+# Function to calculate the mean of an array
+mean() {
+    local sum=0
+    local count=0
+    for value in "$@"; do
+        sum=$((sum + value))
+        count=$((count + 1))
+    done
+    echo "scale=3; $sum / $count" | bc
+}
+
+# Function to calculate the maximum value in an array
+max() {
+    local max_value=$1
+    shift
+    for value in "$@"; do
+        if (( $(echo "$value > $max_value" | bc -l) )); then
+            max_value=$value
+        fi
+    done
+    echo "$max_value"
+}
+
+# Main function to measure system delay
+measure_system_delay() {
+    local avgs=()
+
+    echo -en "${LIGHT_BLUE}⏳ Measuring system delay for 200ms usleep on this machine"
+    for i in {1..20}; do
+        echo -n "."
+        avgs+=($(measure))
+    done
+    echo -e "${RESET}\n"
+
+    local avg_delay=$(mean "${avgs[@]}")
+    local peak_delay=$(max "${avgs[@]}")
+
+    echo "[1] Average delay: ${avg_delay}ms"
+    echo "[2] Peak delay: ${peak_delay}ms"
+
+    if (( $(echo "$avg_delay > 10" | bc -l) )); then
+        echo -e "${YELLOW}Your machine delay is more than 10ms ${RESET}"
+        sleep 5
+    fi
+	echo -e "\n"
+}
+
+# Run the measurement
+measure_system_delay
+
+rm usleep.c usleep
---
+++ philotester.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+
+# Text Styles
+BOLD="\e[1m"
+RESET="\033[0m"
+# Colors
+BLACK="\033[38;5;0m"        # Black
+GREY="\033[38;5;250m"
+DARK_BLUE="\033[38;5;25m"   # Dracula-inspired dark blue
+LIGHT_BLUE="\033[38;5;39m"  # Bright but not overwhelming light blue
+PINK="\033[38;5;213m"       # Soft pink
+RED="\033[38;5;196m"        # Bright red for errors or warnings
+ORANGE="\033[38;5;208m"     # Vibrant orange
+YELLOW="\033[38;5;220m"     # Warm yellow
+GREEN="\033[38;5;40m"       # Fresh green
+WHITE="\033[38;5;15m"       # Pure white
+PURPLE="\033[38;5;129m"     # Dracula-inspired purple (optional addition)
+
+
+# PATH
+EXEC_PATH="../../thread_mutex"					# Customize
+EXEC="${EXEC_PATH}/philo"						# Customize
+CASES_PATH="cases/"
+if [[ ! -d "logs" ]]; then
+	mkdir logs
+else
+	rm logs/*
+fi
+LOGS_PATH="logs"
+
+# VALGRIND
+if [[ ! -f "${LOGS_PATH}/valgrind.log" ]] ; then
+	touch ${LOGS_PATH}/valgrind.log; fi
+if [[ ! -f "valgrind.supp" ]] ; then
+	touch "valgrind.supp"; fi
+VALG_LOG=${LOGS_PATH}/valgrind.log
+VALG_SUP="valgrind.supp"
+VALGRIND="valgrind -q \
+--show-leak-kinds=all --error-limit=no \
+--suppressions=$VALG_SUP \
+--log-file=$VALG_LOG"
+
+# FLAGS
+flag_valgrind=false					# Customize
+while getopts "v" flag; do
+	case $flag in
+		v) flag_valgrind=true ;; 		# Customize
+	esac
+done
+
+# CASES
+TOTAL=0
+FAILED=0
+if [[ $# -gt 0 ]]; then
+	cases=("$@")
+else
+	cases=($(ls $CASES_PATH)); fi
+
+# HEADER
+print_header() {
+	HEADER_NAME="DINING PHILOSOPHERS TESTER"	# Customize
+	HEADER_COLOR=$LIGHT_BLUE						# Customize
+	HEADER_EMOJI="🥢"							# Customize
+	HEADER_SEP="-"
+	LINE_LENGTH=$(tput cols)
+	HEADER_TEXT="${HEADER_EMOJI} ${HEADER_NAME} ${HEADER_EMOJI}"
+	SEPARATOR=$(printf "%-${LINE_LENGTH}s" "" | tr " " "-")
+	HEADER_TEXT_LENGTH=${#HEADER_TEXT}
+	PADDING=$(( (LINE_LENGTH - HEADER_TEXT_LENGTH) / 2 ))
+	echo -e "\n${HEADER_COLOR}${BOLD}${SEPARATOR}${RESET}\n"
+	printf "${HEADER_COLOR}${BOLD}%*s%s%*s${RESET}\n" $PADDING "" "$HEADER_TEXT" $PADDING ""
+	echo -e "${HEADER_COLOR}${BOLD}${SEPARATOR}${RESET}\n"
+}
+
+# NORM (optional)
+check_norm() {
+	echo -e "${HEADER_EMOJI}${HEADER_COLOR} Testing: build and norm${RESET}"
+	make -C "$EXEC_PATH" fclean > /dev/null 2>&1
+	norminette $EXEC_PATH > /dev/null 2>&1
+	if [[ $? -eq 0 ]]; then
+		echo -e "${GREEN}✅ Norminette ok${RESET}"
+	else
+		echo -e "${RED}💀 Norminette failed...${RESET}"
+	fi
+}
+
+# BUILD
+build_program() {
+
+	BUILD_FAIL=$(make -C "$EXEC_PATH" re > /dev/null 2>&1)
+	if [[ $? -eq 0 ]]; then
+		echo -e "${GREEN}✅ Build sucessfully\n${RESET}"
+	else
+		echo -e "${RED}💀 Build failed..${RESET}"
+		echo "$BUILD_FAIL"
+		exit 1
+	fi
+}
+
+run_cases() {
+	for case in ${cases[@]}; do
+		echo -e "${HEADER_EMOJI}${HEADER_COLOR} Testing: ${case}${RESET}"
+		run_tests "${case}"
+		echo -e "\n"
+	done
+}
+
+run_tests() {
+    case="$1"
+    test_file="${CASES_PATH}/${case}"  # Correct path to the test file
+    counter=1  # Counter for log files
+
+    # Check if the test file exists
+    if [[ ! -f "$test_file" ]]; then
+        echo -e "${RED}💀 Test file not found: ${test_file}${RESET}"
+        return
+    fi
+
+    while IFS= read -r test_case && IFS= read -r expected_values; do
+        [[ -z "$test_case" || -z "$expected_values" ]] && continue
+
+        # Generate log file name with counter
+        log_file="${LOGS_PATH}/${case}_${counter}.log"
+
+        # Run the program with the test case
+        if $flag_valgrind; then
+            output=$($VALGRIND $EXEC $test_case 2>&1)
+        else
+            output=$($EXEC $test_case 2>&1)
+        fi
+
+        # Log the output
+        echo "$output" > "$log_file"
+
+        # Extract actual stats from the log output
+        actual_deaths=$(grep -c "died" "$log_file")
+        actual_meals=$(grep -c "is eating" "$log_file")
+        actual_sleeps=$(grep -c "is sleeping" "$log_file")
+        actual_thinks=$(grep -c "is thinking" "$log_file")
+
+        # Extract expected stats
+        read -r expected_deaths expected_meals expected_sleeps expected_thinks <<< "$expected_values"
+
+        # Compare actual vs expected
+        if [[ "$actual_deaths" -le "$expected_deaths" \
+              && "$actual_meals" -eq "$expected_meals" \
+              && "$actual_sleeps" -eq "$expected_sleeps" \
+              && "$actual_thinks" -eq "$expected_thinks" ]]; then
+            echo -e "${GREEN}[$counter] ${WHITE}./philo ${test_case}${RESET}"
+        else
+            echo -e "${RED}[$counter] ./philo ${test_case}${RESET}"
+            echo -e "${GREY}  Deaths   : $actual_deaths (actual) vs $expected_deaths (expected)"
+            echo "  Meals    : $actual_meals (actual) vs $expected_meals (expected)"
+            echo "  Sleeps   : $actual_sleeps (actual) vs $expected_sleeps (expected)"
+            echo -e "  Thinks   : $actual_thinks (actual) vs $expected_thinks (expected)${RESET}"
+            ((FAILED++))
+        fi
+        ((TOTAL++))
+		((counter++))
+    done < "$test_file"
+}
+
+print_result() {
+	PASSED=$((TOTAL - FAILED))
+	if [[ "$PASSED" -eq "$TOTAL" ]] ; then
+		RES_EMOJI="🎉"
+		RES_COLOR=$GREEN
+	else
+		RES_EMOJI="☠️"
+		RES_COLOR=$RED
+	fi
+	RESULT="${RES_EMOJI}  ${PASSED}/$TOTAL ${RES_EMOJI}"
+	RESULT_LENGTH=${#RESULT}
+	PADDING=$(( (LINE_LENGTH - RESULT_LENGTH) / 2 ))
+	echo -e "${RES_COLOR}${BOLD}${SEPARATOR}${RESET}\n"
+	printf "${RES_COLOR}${BOLD}%*s%s%*s${RESET}\n" $PADDING "" "$RESULT" $PADDING ""
+	echo -e "${RES_COLOR}${BOLD}${SEPARATOR}${RESET}\n"
+}
+
+
+# RUN SCRIPT
+print_header
+source ./measure_delay.sh
+check_norm
+build_program
+run_cases
+print_result
