f4026ef70dd40932b1fe687e282efbb16c8b6e9e
VICXPTRV <viktoriavorontsova.main@gmail.com>
2025-03-30 14:42:58 +0700

    fix: remove meals error for 1 philo

--- src/validator/act_eat.sh
+++ src/validator/act_eat.sh
@@ -24,6 +24,13 @@ validate_meals_eaten() {
 	if [[ -z $meals_to_eat ]]; then
 		return
 	fi
+	if [[ $number_of_philos -eq 1 ]]; then
+		if [[ $meals_eaten -gt 0 ]]; then
+			TEST_MSG="Philo $philo: shouldn't eat, but ate $meals_eaten times."
+			F_FAIL=true
+		fi
+		return
+	fi
 	if [[ $meals_eaten -lt $meals_to_eat ]]; then
 		TEST_MSG="Philo $philo: Should eat at least $meals_to_eat times, but ate $meals_eaten"
 		F_FAIL=true
